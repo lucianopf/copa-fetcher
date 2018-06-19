14363a2d1c0f6c15a6bfe39560658e146a1660fe
Luciano Franca <lucianopf@outlook.com>
2018-06-19 12:56:06 -0300

    reconnect to WS

--- index.js
+++ index.js
@@ -3,21 +3,30 @@ const WebSocket = require('websocket').client
 let cachedData = []
 
 const wsUri = 'wss://rtw.uol.com/sub?id=app-esporte-placar-futebol-live-worldcup'
-const websocket = new WebSocket()
-websocket.on('connect', (connection) => {
-  console.log('Connection to WS again')
-  connection.on('message', (message) => {
-    console.log('New message')
-    if (message.type === 'utf8') {
-      let jsonData = JSON.parse(message.utf8Data).subchannels
-      cachedData = Object.keys(jsonData).map(k => jsonData[k])
-    }
+
+function initializeWebSocket (wsUri) {
+  const websocket = new WebSocket()
+  websocket.on('connect', (connection) => {
+    connection.on('message', (message) => {
+      if (message.type === 'utf8') {
+        let jsonData = JSON.parse(message.utf8Data).subchannels
+        cachedData = Object.keys(jsonData).map(k => jsonData[k])
+      }
+    })
+    connection.on('error', (error) => {
+      console.log('Connection Error: ' + error.toString())
+    })
+    connection.on('close', () => {
+      initializeWebSocket(wsUri)
+    })
   })
-})
-websocket.connect(wsUri)
+  websocket.connect(wsUri)
+  return websocket
+}
+
+initializeWebSocket(wsUri)
 
 module.exports = (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  websocket.connect(wsUri)
   return cachedData
 }
