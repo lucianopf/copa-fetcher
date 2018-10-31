adf7d78e3629fe7f99e6ff3949fa0114933ad3d6
Lucas Sartori <lucas.sartm@gmail.com>
2018-10-31 17:28:34 -0300

    added prettier

--- index.js
+++ index.js
@@ -2,18 +2,19 @@ const WebSocket = require('websocket').client
 
 let cachedData = []
 
-const wsUri = 'wss://rtw.uol.com/sub?id=app-esporte-placar-futebol-live-worldcup'
+const wsUri =
+  'wss://rtw.uol.com/sub?id=app-esporte-placar-futebol-live-worldcup'
 
-function initializeWebSocket (wsUri) {
+function initializeWebSocket(wsUri) {
   const websocket = new WebSocket()
-  websocket.on('connect', (connection) => {
-    connection.on('message', (message) => {
+  websocket.on('connect', connection => {
+    connection.on('message', message => {
       if (message.type === 'utf8') {
         let jsonData = JSON.parse(message.utf8Data).subchannels
         cachedData = Object.keys(jsonData).map(k => jsonData[k])
       }
     })
-    connection.on('error', (error) => {
+    connection.on('error', error => {
       console.log('Connection Error: ' + error.toString())
     })
     connection.on('close', () => {
